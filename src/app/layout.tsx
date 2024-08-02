13abaaa397142ba7d2cac320c30e5523e9117f9b
Matthew Jordan <mdj82@georgetown.edu>
2024-08-01 20:46:34 -0400

    Added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
 import Navbar from '@/components/Navbar/Navbar';
 import Footer from '@/components/Footer/Footer';
 
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
+
 export const metadata: Metadata = {
   title: 'College Compass',
   description: 'Find your perfect college (student) advisor.',
