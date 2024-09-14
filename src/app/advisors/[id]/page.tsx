d94b35a1f3ced37839fd941d8927c1b0d6b2a470
Matthew Jordan <mdj82@georgetown.edu>
2024-09-14 11:56:40 -0400

    Fixed calendly embed

--- src/app/advisors/[id]/page.tsx
+++ src/app/advisors/[id]/page.tsx
@@ -1,5 +1,5 @@
-import CalendlyEmbed from "@/components/Calendly/CalendlyEmbed";
-import { getCalendlyUrlFromAdvisorId } from "@/lib/queries";
+import CalendlyEmbed from '@/components/Calendly/CalendlyEmbed';
+import { getCalendlyUrlFromAdvisorId } from '@/lib/queries';
 
 export const revalidate = 0;
 
@@ -10,23 +10,24 @@ interface Props {
 }
 
 export default async function DisplayCalendly({ params }: Props) {
-
   const calendly_url = await getCalendlyUrlFromAdvisorId(params.id);
 
   return (
     <div
       style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '100%',
-      height: '100vh',
-      boxSizing: 'border-box',
-      padding: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100vh',
+        boxSizing: 'border-box',
+        padding: '20px',
       }}
     >
-      <h1 style={{ marginBottom: '20px', textAlign: 'center' }}>Schedule an Appointment</h1>
+      <h1 style={{ marginBottom: '20px', textAlign: 'center' }}>
+        Schedule an Appointment
+      </h1>
       <CalendlyEmbed
         url={calendly_url}
         style={{ width: '100%', height: '80%', border: 'none' }}
--- src/components/Calendly/CalendlyEmbed.tsx
+++ src/components/Calendly/CalendlyEmbed.tsx
@@ -1,27 +1,35 @@
-"use client";
+'use client';
 
-import React, { useEffect } from "react";
+import React, { useEffect, CSSProperties } from 'react';
 
 interface CalendlyEmbedProps {
   url: string;
+  style?: CSSProperties;
 }
 
-const CalendlyEmbed: React.FC<CalendlyEmbedProps> = ({ url }) => {
+const CalendlyEmbed: React.FC<CalendlyEmbedProps> = ({ url, style }) => {
   useEffect(() => {
-    const head = document.querySelector("head");
-    const script = document.createElement("script");
+    const head = document.querySelector('head');
+    const script = document.createElement('script');
     script.setAttribute(
-      "src",
-      "https://assets.calendly.com/assets/external/widget.js"
+      'src',
+      'https://assets.calendly.com/assets/external/widget.js',
     );
     head?.appendChild(script);
   }, []);
+
+  const defaultStyle: CSSProperties = {
+    minHeight: '650px',
+    width: '100%',
+  };
+
+  const combinedStyle = { ...defaultStyle, ...style };
 
   return (
     <div
       className="calendly-inline-widget"
       data-url={url}
-      style={{ minHeight: "650px", width: "100%" }}
+      style={combinedStyle}
     ></div>
   );
 };
