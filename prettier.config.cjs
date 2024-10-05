e4570d466fe58cccd198fd6421cdc16cab9987ae
Thomas Forbes <34174315+thomas-forbes@users.noreply.github.com>
2024-10-04 21:24:36 -0400

    chore/setup-changes (#3)
    * tailwind + prettier
    
    * move prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
