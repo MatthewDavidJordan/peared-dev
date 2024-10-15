2c238b96d4a9b2d3f28f18368b798b63afa66334
joshuahqy <86606372+joshuahqy@users.noreply.github.com>
2024-10-15 19:10:51 -0400

    Lineartasks10-15 (#24)
    * Returns advisor with meeting
    
    * Typing cleanup
    
    * Adjusted routes and added otp

--- src/app/api/advisor/[id]/route.ts
+++ src/app/api/advisor/[id]/route.ts
@@ -1,13 +1,10 @@
 import { NextResponse } from 'next/server';
-import { getMeetingById, getAdvisorById, Meeting, Advisor } from '@/lib/queries';
+import { Advisor, getAdvisorById } from '@/lib/queries';
 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
   try {
-    const meeting: Meeting | null = await getMeetingById(Number(params.id));
-
-    const advisor: Advisor | null = await getAdvisorById(meeting!.advisor_id!);
-
-    return NextResponse.json({ meeting, advisor }, { status: 200 });
+    const advisor: Advisor | null = await getAdvisorById(Number(params.id));
+    return NextResponse.json(advisor, { status: 200 });
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
@@ -15,4 +12,3 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
     return NextResponse.json({ error: 'An unknown error occurred' }, { status: 500 });
   }
 }
-
