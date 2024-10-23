31854e7290af4970eb20cc8b86ee70b579e4749d
Thomas Forbes <34174315+thomas-forbes@users.noreply.github.com>
2024-10-23 19:17:09 -0400

    fix/frontend's-be-dependencies (#38)

--- src/app/book/[advisor_id]/BookCard.tsx
+++ src/app/book/[advisor_id]/BookCard.tsx
@@ -3,14 +3,16 @@ import AdvisorPreview from '@/app/book/[advisor_id]/AdvisorPreview';
 import SignUpForm from '@/app/book/[advisor_id]/SignUpForm';
 import TimeForm from '@/app/book/[advisor_id]/TimeForm';
 import { cn } from '@/lib/funcs';
-import type { AvailabilityEvent, getAdvisorById } from '@/lib/queries';
+import type { AvailabilityEvent, getAdvisorById, getCollegeById } from '@/lib/queries';
 import { parseAsIsoDateTime, useQueryState } from 'nuqs';
 
 export default function BookCard({
   advisor,
+  school,
   availabilities,
 }: {
   advisor: Awaited<ReturnType<typeof getAdvisorById>>;
+  school: Awaited<ReturnType<typeof getCollegeById>>;
   availabilities: AvailabilityEvent[];
 }) {
   const [selectedTime, setSelectedTime] = useQueryState('time', parseAsIsoDateTime);
@@ -21,7 +23,7 @@ export default function BookCard({
         'flex flex-col items-stretch divide-y rounded-md border shadow-lg lg:flex-row lg:divide-x lg:divide-y-0 [&>*]:lg:w-72',
       )}
     >
-      <AdvisorPreview advisor={advisor} />
+      <AdvisorPreview advisor={advisor} school={school} />
       {!selectedTime ? (
         <TimeForm
           availabilities={availabilities}
