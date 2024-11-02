832040575ebca3a6496612b08a2739203bd87c64
joshuahqy <joshua.hqy@gmail.com>
2024-11-02 16:13:16 -0400

    Uses service account

--- src/lib/googleMeet.ts
+++ src/lib/googleMeet.ts
@@ -1,20 +1,19 @@
 import { google } from "googleapis";
+import * as serviceAccount from "./service-account-key.json";
 
-// Define the OAuth2 client with the admin credentials
-const oauth2Client = new google.auth.OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-  'urn:ietf:wg:oauth:2.0:oob'
+
+const jwtClient = new google.auth.JWT(
+  serviceAccount.client_email,
+  undefined,
+  serviceAccount.private_key,
+  ['https://www.googleapis.com/auth/calendar']
 );
-
-// Set the refresh token
-oauth2Client.setCredentials({
-  refresh_token: process.env.REFRESH_TOKEN,
-});
 
 export default async function createGoogleMeetWithParticipants(startTime: string, endTime: string, advisorEmail: string, studentEmail: string): Promise<string> {
   try {
-    const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+    await jwtClient.authorize();
+
+    const calendar = google.calendar({ version: 'v3', auth: jwtClient });
 
     // Define the event
     const event = {
