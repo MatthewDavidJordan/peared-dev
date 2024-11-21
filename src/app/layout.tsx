30ead00fea7fc4414457af0746146731fb8530a1
Matthew Jordan <mdj82@georgetown.edu>
2024-11-21 13:12:38 -0500

    updated favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,24 @@ import type { Metadata } from 'next';
 import React from 'react';
 import './globals.css';
 
+import iconLignt from '@/public/LightMode.ico';
+import iconDark from '@/public/images/favicon.ico';
+
 export const metadata: Metadata = {
   title: 'peared',
   description: 'Find your perfect college (student) advisor.',
-  icons: {
-    icon: '/favicon.ico',
-  },
+  icons: [
+    {
+      media: '(prefers-color-scheme: light)',
+      url: iconLignt.src,
+      type: 'image/svg+xml',
+    },
+    {
+      media: '(prefers-color-scheme: dark)',
+      url: iconDark.src,
+      type: 'image/svg+xml',
+    },
+  ],
 };
 
 export default function RootLayout(props: { children: React.ReactNode }) {
