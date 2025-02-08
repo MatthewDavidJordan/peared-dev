47adbba7d80989f45c0989bbe6bf1a14c9697f97
Matthew Jordan <mdj82@georgetown.edu>
2025-02-08 13:08:59 -0500

    added stand in privacy policy

---
+++ src/app/legal/privacy-policy/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export default async function PrivacyPolicy() {
+  return (
+    <div>
+      <h1>Peared Privacy Notice</h1>
+      <p>Last Updated: February 2025</p>
+      <section className="mt-6">
+        <h2 className="text-2xl font-semibold">Welcome</h2>
+        <p>
+          Peared Inc. (&quot;Peared&quot;, &quot;we&quot;, &quot;us&quot;, &quot;our&quot;) respects
+          your privacy. This privacy notice (&quot;Privacy Notice&quot;) has been adopted on a
+          company-wide basis to guide how we process the information that you provide to us through
+          the Peared website, our user portal accessible through www.peared.org, and services
+          accessible through the user portal (collectively, the &quot;Services&quot;).
+        </p>
+      </section>
+
+      <section className="mt-6">
+        <h2 className="text-2xl font-semibold">Lawful Basis for Processing</h2>
+        <p>
+          Many jurisdictions require that we disclose to you the lawful basis for our processing of
+          your personal information. We do that throughout this Privacy Notice.
+        </p>
+      </section>
+
+      <section className="mt-6">
+        <h2 className="text-2xl font-semibold">Information We Process</h2>
+        <p>
+          You do not need to proactively provide any personal information to view our website, but
+          you will have to provide required personal information to fully use our services.
+        </p>
+      </section>
+
+      <section className="mt-6">
+        <h2 className="text-2xl font-semibold">How We Protect Your Personal Information</h2>
+        <p>
+          We take reasonable steps to maintain the security of your personal information; however,
+          no data transmission over the Internet can be guaranteed to be completely secure.
+        </p>
+      </section>
+
+      <section className="mt-6">
+        <h2 className="text-2xl font-semibold">Contact Us</h2>
+        <p>If you have any questions, please email us at support@peared.org.</p>
+      </section>
+    </div>
+  );
+}
